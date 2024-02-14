c4d1defb5852521c79ed666f1de1a48e92041780
escot23 <danielaescoto6@gmail.com>
2024-02-14 17:57:31 -0600

    Agregando mi WORKSHOP 2

--- Workshop2/server/index.js
+++ Workshop2/server/index.js
@@ -1,32 +1,29 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
-// database connection
-const mongoose = require("mongoose");
-const db = mongoose.connect("mongodb://127.0.0.1:27017/todo-api");
 
-const {
-  taskPatch,
-  taskPost,
-  taskGet,
-} = require("./controllers/taskController.js");
-
-// parser for the request body (required for the POST and PUT methods)
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
 
 // check for cors
-const cors = require("cors");
 app.use(cors({
   domains: '*',
   methods: "*"
 }));
 
 
-// listen to the task request
-app.get("/api/tasks", taskGet);
-app.post("/api/tasks", taskPost);
-app.patch("/api/tasks", taskPatch);
-app.put("/api/tasks", taskPatch);
+// listen to GET requests on /hello
+app.get('/hello', function (req, res) {
+  res.send('Hello World');
+});
+
+
+app.get('/tipocambio', function (req, res) {
+  res.send(`{
+    "TipoCompraDolares" : "608",
+    "TipoVentaDolares" : "621",
+    "TipoCompraEuros" : "731.85",
+    "TipoVentaEuros" : "761.9"
+  }`);
+});
 
 
 app.listen(3000, () => console.log(`Example app listening on port 3000!`))
